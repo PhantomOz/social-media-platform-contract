ed6845c9c2542e24230b587e63875c88b616df20
PhantomOz <faniogor@gmail.com>
2024-02-27 17:56:47 +0100

    This add like and comment

--- contracts/SocialMedia.sol
+++ contracts/SocialMedia.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.19;
 error UserAlreadyExists();
 error Unauthorised();
 error URI_TooShort();
+error BAD_REQUEST();
 
 /// @title A decentralized social media platform
 /// @author Favour Aniogor
@@ -33,10 +34,14 @@ contract SocialMedia {
         string _description;
         address _author;
         uint256 _likes;
+        uint256 _commentCounts;
         uint256 _createdAt;
     }
     struct Comment {
-        string message;
+        string _comment;
+        uint256 _messagId;
+        address _author;
+        uint256 _createdAt;
     }
 
     function registerUser(string memory _username) external {
@@ -46,6 +51,7 @@ contract SocialMedia {
         addressToUser[msg.sender] = User(_username, 0, 0, 0, true, 0, block.timestamp);
         //emit an event
     }
+
     function postContent(string memory _description, string calldata _tokenUri) external {
         if(!addressToUser[msg.sender]._isExists){
             revert Unauthorised();
@@ -54,11 +60,38 @@ contract SocialMedia {
             revert URI_TooShort();
         }
         //mint nft and change tokenId in the Message Struct
-        idToMessage[messageCounter] = Message(messageCounter, _description,msg.sender, 0, block.timestamp);
+        idToMessage[messageCounter] = Message(messageCounter, _description,msg.sender, 0, 0, block.timestamp);
         //emit event of post
+        messageCounter++;
     }
-    function likeContent() external {}
-    function commentOnContent() external {}
+
+    function toggleLikeContent(uint256 _messageId) external {
+        if(!addressToUser[msg.sender]._isExists){
+            revert Unauthorised();
+        }
+        if(idToMessage[_messageId]._createdAt == 0){
+            revert BAD_REQUEST();
+        }
+        if(!messageIdToLikes[_messageId][msg.sender]){
+            idToMessage[_messageId]._likes += 1;
+        }else{
+            idToMessage[_messageId]._likes -= 1;
+        }
+        //emit like event
+    }
+
+    function commentOnContent(uint256 _messageId, string calldata _comment) external {
+        if(!addressToUser[msg.sender]._isExists){
+            revert Unauthorised();
+        }
+        if(idToMessage[_messageId]._createdAt == 0){
+            revert BAD_REQUEST();
+        }
+        uint256 _count = idToMessage[_messageId]._commentCounts;
+        messageIdToComment[_messageId][_count] = Comment(_comment, _messageId, msg.sender, block.timestamp);
+        idToMessage[_messageId]._commentCounts += 1;
+        //emit event here
+    }
     function followUser() external {}
     function unFollowUser() external {}
     function shareContent() external {}
